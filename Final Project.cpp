249c9aab9aded0234b96804eecda2e49903643d2
Nics Kirschtein <94561281+nikonics@users.noreply.github.com>
2021-12-03 03:52:25 +0800

    Update Final Project.cpp

--- Final Project.cpp
+++ Final Project.cpp
@@ -4,40 +4,68 @@ using namespace std;
 
 int main ()
 {
+
+    //DESIGN SECTION
+    char *A = "--------------------------------------------------";
+    char *B = "\n    -------------------------------------         ";
+
+	
+	
+    //MAIN INPUT
     int x;
-    string a;
+    bool tryagain = false;
     string user;
     string pass;
 
-    cout << "== [ FOOD DELIVERY APP ] == \n";
-    cout << "== You must Login First to order ==\n";
+
+    cout << A;
+    cout << "\n              FOOD DELIVERY APP        ";
     cout << "\n";
+    cout << A;
 
-    cout << "Username: ";
+    cout << "\n";
+    cout << B;
+    cout << "\n    [!] You must Login First to order [!]";
+    cout << B;
+    cout << "\n\n";
+
+	
+	
+	
+    // LOGIN FORM
+do {
+    cout << "         Username: ";
     cin >> user;
-    cout << "Password: ";
+    cout << "         Password: ";
     cin >> pass;
-
 
       if (user == "nics" && pass == "123") {
            cout << "\n";
-	       cout << "***Successfully Logged in!***\n\n";
+	       cout << "     ***Successfully Logged in!***\n\n";
+	       tryagain = true;
       }
 
          else if (user == "nics") {
            cout << "\n";
-           cout << "[!] The password is incorrect! [!]\n\n";
+           cout << "     [!] The password is incorrect! Try Again...[!]\n\n";
+           cout << "\n";
          }
 
          else if (pass == "123") {
             cout << "\n";
-            cout << "[!] The username is incorrect! [!]\n\n";
+            cout << "     [!] The username is incorrect! Try Again...[!]\n\n";
+           cout << "\n";
             }
 
 	     else {
             cout << "\n";
-            cout << "[!] Invalid username and password! Please Try Again... [!]\n\n";
+            cout << "  [!] Invalid username and password! Please Try Again... [!]\n\n";
+
+
+           cout << "\n";
 	     }
+ }
+ while (!tryagain);
 
 
 return 0;
