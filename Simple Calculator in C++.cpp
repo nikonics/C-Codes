7123163701455d2ea4470533115bee8982d18a28
Nics Kirschtein <94561281+nikonics@users.noreply.github.com>
2022-01-03 21:17:44 +0800

    Update Simple Calculator in C++.cpp

--- Simple Calculator in C++.cpp
+++ Simple Calculator in C++.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main ()
 {
     int digit1, digit2;
-    char selection;
+    int selection;
+    char beginning;
 
+{
+    start:
+    system ("cls");
     cout << "======================="<<endl;
     cout << "Enter Number 1: ";
     cin >> digit1;
@@ -15,37 +19,44 @@ int main ()
     cout << "\n\n";
 
     cout << "** Type Any Operator you want to use **\n\n";
-    cout << "[+]: Addition"<<endl;
-    cout << "[-]: Subtraction"<<endl;
-    cout << "[*]: Multiplication"<<endl;
-    cout << "[/]: Division\n"<<endl;
+    cout << " 1) Addition"<<endl;
+    cout << " 2) Subtraction"<<endl;
+    cout << " 3) Multiplication"<<endl;
+    cout << " 4) Division\n"<<endl;
+
     cout << "Type an Operator: ";
     cin >> selection;
 
     cout <<"\n\n";
-
     switch (selection) {
-    case '+':
+    case 1:
         cout << "Result: " << (digit1 + digit2);
         break;
 
-    case '-':
+    case 2:
         cout << "Result: " << (digit1 - digit2);
         break;
 
-    case '*':
+    case 3:
         cout << "Result: " << (digit1 * digit2);
         break;
 
-    case '/':
+    case 4:
         cout << "Result: " << (digit1 / digit2);
         break;
 
     default:
         cout << "[ " <<selection << " ]" << " is an Invalid!"<<endl;
-    }
 
-// Nics Kirschtein //
+}
+    cout << "\n\n\n\n";
+    cout << "Do you want to try again? [Y / N]: "<<endl;
+    cin >> beginning;
+    if (beginning == 'Y' || beginning == 'y') {
+    goto start;
+   }
+
+}
 
 return 0;
 }
